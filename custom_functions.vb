6cbf15e93a3f552c6c0cbf24d0d36fb27c2b9c66
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2017-02-27 13:18:05 +0530

    added Exit For and limited for loop to 1000

--- custom_functions.vb
+++ custom_functions.vb
@@ -176,9 +176,10 @@ Function NAG_HSEARCH(rng As Range, str As String, vOffset As Double) As Range
 	Application.Volatile True
 	Dim i, sCol, sRow As Integer
 	sCol = 0
-	For i = 1 To rng.Columns.Count
+	For i = 1 To rng.Columns.Count And i < 1000
 		If rng.Cells(1, 1).Offset(0, i - 1).Value = str Then
 			sCol = i
+			Exit For
 		End If
 	Next i
 	Set NAG_HSEARCH = rng.Cells(vOffset + 1, sCol)
@@ -189,59 +190,65 @@ Function NAG_TABLE_SEARCH(hRng As Range, hStr As String, vRng As Range, vStr As 
 	Dim i, sCol, sRow As Integer
 	sCol = 0
 	sRow = 0
-	For i = 1 To hRng.Columns.Count
+	For i = 1 To hRng.Columns.Count And i < 1000
 		If hRng.Cells(1, 1).Offset(0, i - 1).Value = hStr Then
 			sCol = hRng.Column + i - 1
+			Exit For
 		End If
 	Next i
-	For i = 1 To vRng.Rows.Count
+	For i = 1 To vRng.Rows.Count And i < 1000
 		If vRng.Cells(1, 1).Offset(i - 1, 0).Value = vStr Then
 			sRow = vRng.Row + i - 1
+			Exit For
 		End If
 	Next i
 	Set NAG_TABLE_SEARCH = hRng.Worksheet.Cells(sRow, sCol)
 End Function
 
 Function NAG_HSEARCH_TWO(topRng As Range, topStr As String, botRng As Range, botStr As String, vOffset As Double) As Range
-   Application.Volatile True
-   Dim i, sCol, sRow As Integer
-   sCol = 0
-   sRow = 0
-   For i = 1 To botRng.Columns.Count
-       If botRng.Cells(1, 1).Offset(0, i - 1).Value = botStr And topRng.Cells(1, 1).Offset(0, i - 1).Value = topStr Then
-           sCol = i
-           sRow = 0
-       End If
-   Next i
-   Set NAG_HSEARCH_TWO = botRng.Cells(vOffset + 1, sCol)
+	Application.Volatile True
+	Dim i, sCol, sRow As Integer
+	sCol = 0
+	sRow = 0
+	For i = 1 To botRng.Columns.Count And i < 1000
+		If botRng.Cells(1, 1).Offset(0, i - 1).Value = botStr And topRng.Cells(1, 1).Offset(0, i - 1).Value = topStr Then
+			sCol = i
+			sRow = 0
+			Exit For
+		End If
+	Next i
+	Set NAG_HSEARCH_TWO = botRng.Cells(vOffset + 1, sCol)
 End Function
 
 Function NAG_TABLE_SEARCH_TWO(hRng As Range, hStr As String, hBRng As Range, hBStr As String, vRng As Range, vStr As String) As Range
-   Application.Volatile True
-   Dim i, sCol, sRow As Integer
-   sCol = 0
-   sRow = 0
-   For i = 1 To hBRng.Columns.Count
-       If hBRng.Cells(1, 1).Offset(0, i - 1).Value = hBStr And hRng.Cells(1, 1).Offset(0, i - 1).Value = hStr Then
-           sCol = hBRng.Column + i - 1
-       End If
-   Next i
-   For i = 1 To vRng.Rows.Count
-       If vRng.Cells(1, 1).Offset(i - 1, 0).Value = vStr Then
-           sRow = vRng.Row + i - 1
-       End If
-   Next i
-   Set NAG_TABLE_SEARCH_TWO = hBRng.Worksheet.Cells(sRow, sCol)
+	Application.Volatile True
+	Dim i, sCol, sRow As Integer
+	sCol = 0
+	sRow = 0
+	For i = 1 To hBRng.Columns.Count And i < 1000
+		If hBRng.Cells(1, 1).Offset(0, i - 1).Value = hBStr And hRng.Cells(1, 1).Offset(0, i - 1).Value = hStr Then
+			sCol = hBRng.Column + i - 1
+			Exit For
+		End If
+	Next i
+	For i = 1 To vRng.Rows.Count And i < 1000
+		If vRng.Cells(1, 1).Offset(i - 1, 0).Value = vStr Then
+			sRow = vRng.Row + i - 1
+			Exit For
+		End If
+	Next i
+	Set NAG_TABLE_SEARCH_TWO = hBRng.Worksheet.Cells(sRow, sCol)
 End Function
 
 Function NAG_VSEARCH(rng As Range, str As String, hOffset As Double) As Range
-   Application.Volatile True
-   Dim i, sCol, sRow As Integer
-   sRow = 0
-   For i = 1 To rng.Rows.Count
-       If rng.Cells(1, 1).Offset(i - 1, 0).Value = str Then
-           sRow = i
-       End If
-   Next i
-   Set NAG_VSEARCH = rng.Cells(sRow, hOffset + 1)
+	Application.Volatile True
+	Dim i, sCol, sRow As Integer
+	sRow = 0
+	For i = 1 To rng.Rows.Count And i < 1000
+		If rng.Cells(1, 1).Offset(i - 1, 0).Value = str Then
+			sRow = i
+			Exit For
+		End If
+	Next i
+	Set NAG_VSEARCH = rng.Cells(sRow, hOffset + 1)
 End Function
