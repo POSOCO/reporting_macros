9e91cd15b8247cdbdd1ebd438d1e1b67d52fb5d7
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2017-02-27 22:52:50 +0530

    added iteration constraint to 1000

--- custom_functions.vb
+++ custom_functions.vb
@@ -177,6 +177,9 @@ Function NAG_HSEARCH(rng As Range, str As String, vOffset As Double) As Range
 	Dim i, sCol, sRow As Integer
 	sCol = 0
 	For i = 1 To rng.Columns.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If rng.Cells(1, 1).Offset(0, i - 1).Value = str Then
 			sCol = i
 			Exit For
@@ -191,12 +194,18 @@ Function NAG_TABLE_SEARCH(hRng As Range, hStr As String, vRng As Range, vStr As 
 	sCol = 0
 	sRow = 0
 	For i = 1 To hRng.Columns.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If hRng.Cells(1, 1).Offset(0, i - 1).Value = hStr Then
 			sCol = hRng.Column + i - 1
 			Exit For
 		End If
 	Next i
 	For i = 1 To vRng.Rows.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If vRng.Cells(1, 1).Offset(i - 1, 0).Value = vStr Then
 			sRow = vRng.Row + i - 1
 			Exit For
@@ -211,6 +220,9 @@ Function NAG_HSEARCH_TWO(topRng As Range, topStr As String, botRng As Range, bot
 	sCol = 0
 	sRow = 0
 	For i = 1 To botRng.Columns.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If botRng.Cells(1, 1).Offset(0, i - 1).Value = botStr And topRng.Cells(1, 1).Offset(0, i - 1).Value = topStr Then
 			sCol = i
 			sRow = 0
@@ -226,12 +238,18 @@ Function NAG_TABLE_SEARCH_TWO(hRng As Range, hStr As String, hBRng As Range, hBS
 	sCol = 0
 	sRow = 0
 	For i = 1 To hBRng.Columns.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If hBRng.Cells(1, 1).Offset(0, i - 1).Value = hBStr And hRng.Cells(1, 1).Offset(0, i - 1).Value = hStr Then
 			sCol = hBRng.Column + i - 1
 			Exit For
 		End If
 	Next i
 	For i = 1 To vRng.Rows.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If vRng.Cells(1, 1).Offset(i - 1, 0).Value = vStr Then
 			sRow = vRng.Row + i - 1
 			Exit For
@@ -245,6 +263,9 @@ Function NAG_VSEARCH(rng As Range, str As String, hOffset As Double) As Range
 	Dim i, sCol, sRow As Integer
 	sRow = 0
 	For i = 1 To rng.Rows.Count
+		If i > 1000 Then
+			Exit For
+		End If
 		If rng.Cells(1, 1).Offset(i - 1, 0).Value = str Then
 			sRow = i
 			Exit For
